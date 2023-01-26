189ca6c43aac8c7405cfee6774541011cc5237ef
Tiftik <ic22b056@technikum-wien.at>
2023-01-26 18:34:36 +0100

    Initial commit

---
+++ src/main/java/com/example/quadrieren/QuadrierenApplication.java
@@ -0,0 +1,13 @@
+package com.example.quadrieren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrierenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadrierenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quadrieren/QuadrierenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadrieren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadrierenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
